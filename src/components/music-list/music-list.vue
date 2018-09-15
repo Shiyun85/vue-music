fc029f3541d5e780685b4965c028db3eceae97e2
shiyun <guzhiyun@gmail.com>
2018-09-15 09:54:28 +0800

    增加loading

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImg">
-      <div class="filter"></div>
+      <div class="play-wrapper" v-if="songs.length > 0" ref="playBtn">
+        <div class="play">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+      <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-player" ref="bgLayer"></div>
     <scroll :data="songs" class="list" ref="list" :probe-type="probeType" :listen-scroll="listenScroll" @scroll="scroll">
       <div class="song-list-wraper">
         <song-list :songs="songs"></song-list>
+      </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
       </div>
     </scroll>
   </div>
@@ -19,7 +28,11 @@
 <script>
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+import Loading from 'base/loading/loading'
+import { prefixStyle } from 'common/js/dom'
 const RESERVED_HEIGHT = 40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 export default {
   props: {
     // 确定哪些数据是需要父组件传递
@@ -58,6 +71,9 @@ export default {
   methods: {
     scroll(pos) {
       this.scrollY = pos.y
+    },
+    back() {
+      this.$router.back()
     }
   },
   watch: {
@@ -65,27 +81,35 @@ export default {
       let translateY = Math.max(this.minTranslateY, newY)
       let zIndex = 0
       let scale = 1
-      this.$refs.bgLayer.style['transform'] = `translate3d(0,${translateY}px,0)`
+      let blur = 0
+      this.$refs.bgLayer.style[transform] = `translate3d(0,${translateY}px,0)`
       const percent = Math.abs(newY / this.imageHeight)
+      console.log('percent', percent)
       if (newY > 0) {
         scale = 1 + percent
         zIndex = 10
+      } else {
+        blur = Math.min(20 * percent, 20)
       }
+      this.$refs.filter.style[backdrop] = `blur(${blur}px)`
       if (newY < this.minTranslateY) {
         zIndex = 10
         this.$refs.bgImg.style.paddingTop = 0
         this.$refs.bgImg.style.height = `${RESERVED_HEIGHT}px`
+        this.$refs.playBtn.style.display = 'none'
       } else {
         this.$refs.bgImg.style.paddingTop = '70%'
         this.$refs.bgImg.style.height = 0
+        this.$refs.playBtn.style.display = 'block'
       }
       this.$refs.bgImg.style.zIndex = zIndex
-      this.$refs.bgImg.style['transform'] = `scale(${scale})`
+      this.$refs.bgImg.style[transform] = `scale(${scale})`
     }
   },
   components: {
     Scroll,
-    SongList
+    SongList,
+    Loading
   }
 }
 </script>
